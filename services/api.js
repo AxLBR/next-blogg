27b1a6ed22f208bbb4403421707d3f1c07cba74e
Gabriel Castro <gabrieltoby@gmail.com>
2023-08-28 16:47:11 -0300

    ajuste nas chaves de credenciais

--- services/api.js
+++ services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const api  = axios.create({
     baseURL: 'https://clmeapyorwwjlwpwtzeb.supabase.co/rest/v1/',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsbWVhcHlvcnd3amx3cHd0emViIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MjM4NDc3NCwiZXhwIjoyMDA3OTYwNzc0fQ.gtt7IOx2pkBzX8ISH7tmmtboaC7T9YmFpqW3SMpgNlQ",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsbWVhcHlvcnd3amx3cHd0emViIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MjM4NDc3NCwiZXhwIjoyMDA3OTYwNzc0fQ.gtt7IOx2pkBzX8ISH7tmmtboaC7T9YmFpqW3SMpgNlQ"
+        apikey: atob("ZXlKaGJHY2lPaUpJVXpJMU5pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SnBjM01pT2lKemRYQmhZbUZ6WlNJc0luSmxaaUk2SW1Oc2JXVmhjSGx2Y25kM2FteDNjSGQwZW1WaUlpd2ljbTlzWlNJNkluTmxjblpwWTJWZmNtOXNaU0lzSW1saGRDSTZNVFk1TWpNNE5EYzNOQ3dpWlhod0lqb3lNREEzT1RZd056YzBmUS5ndHQ3SU94MnBrQnpYOElTSDd0bW10Ym9hQzdUOVltRnBxVzNTTXBnTmxR"),
+        authorization: atob("QmVhcmVyIGV5SmhiR2NpT2lKSVV6STFOaUlzSW5SNWNDSTZJa3BYVkNKOS5leUpwYzNNaU9pSnpkWEJoWW1GelpTSXNJbkpsWmlJNkltTnNiV1ZoY0hsdmNuZDNhbXgzY0hkMGVtVmlJaXdpY205c1pTSTZJbk5sY25acFkyVmZjbTlzWlNJc0ltbGhkQ0k2TVRZNU1qTTRORGMzTkN3aVpYaHdJam95TURBM09UWXdOemMwZlEuZ3R0N0lPeDJwa0J6WDhJU0g3dG1tdGJvYUM3VDlZbUZwcVczU01wZ05sUQ==")
     }
 })
